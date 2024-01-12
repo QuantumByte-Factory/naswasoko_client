2d50da83c3bab226523acfb1eb75254e9129d378
salvatoluice <salvatoluice5@gmail.com>
2024-01-12 12:13:45 +0300

    ft banner

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import banner from '../assets/banner5.jpg'
 
 const Hero = () => {
   return (
     <div className="relative overflow-hidden bg-white">
-      <img className='z-1' src="http://naswasoko.co.ke/static/media/banner6.e80f5820ac319b7fcf74.jpg" alt="" />
+      <img className='z-1' src={banner} alt="" />
       <div className='z-10 gap-[2%] py-[2%] grid grid-cols-2 md:grid-cols-6 mb-3 justify-center bg-gray-50 bg-opacity-70 mx-0 px-[5%] md:px-0 md:mx-[10%] '>
         <div className="bg-white p-3 flex cursor-pointer flex-col shadow-md w-full md:w-[180px] bg-opacity-70 justify-center text-center h-[220px]">
           <p className='text-[20px] font-medium mb-4'>Smartphones</p>
